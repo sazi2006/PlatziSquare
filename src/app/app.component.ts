2e4a7f148b2aeaed581230c65722e5f8e453b55e
Alejandro Zapata <alejo.z3@gmail.com>
2018-11-12 23:17:47 -0500

    Pruebas String Interpolation, Property Binding, Even Binding y Two Way Data Binding (Hasta el vídeo 13)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PlatziSquare';
+  a = 3;
+  b = 5;
+  listo = false;
+  nombre: string = '';
+
+  constructor() {
+    setTimeout(() => {
+      this.listo = true;
+    }, 3000);
+  }
+  hacerAlgo() {
+    alert('Haciendo Algo');
+  }
 }
